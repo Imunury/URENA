2260e08d37594817d709b9867161c8d80a0d3a9f
LeeJS9856 <wnstj1057@naver.com>
2024-08-28 02:51:24 +0900

    feat : home 화면 qna 버튼 추가

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -19,6 +19,7 @@ const Home: React.FC = () => {
                 <Link href='/student'><button className="bg-emerald-400 mt-8 px-10 py-3 rounded-md"><h1>학생 정보</h1></button></Link>
                 <Link href='/moti'><button className="bg-emerald-400 mt-8 px-10 py-3 rounded-md"><h1>모티 정보</h1></button></Link>
                 <Link href='/mission'><button className="bg-emerald-400 mt-8 px-10 py-3 rounded-md"><h1>미션 인증</h1></button></Link>
+                <Link href='/qna'><button className="bg-emerald-400 mt-8 px-10 py-3 rounded-md"><h1>QNA</h1></button></Link>
             </div>
             <div>
                 <button className="bg-gray-500 mt-5 px-5 py-2 rounded-md" onClick={logout}>Log out</button>
